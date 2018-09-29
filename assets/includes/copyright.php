2438e9fd3f50f679512447dc8df89a386d952f0e
Al Mahfuz <almahfuz380@gmail.com>
2018-09-29 18:23:46 +0600

    copyright modified

--- assets/includes/copyright.php
+++ assets/includes/copyright.php
@@ -1,2 +1,1 @@
-<h4>Code Dynamic Websites with PHP</h4>
-<h5>Copyright &copy;<?php echo date('Y');?> <a href="http://bradhussey.ca/" target="_blank">Brad Hussey</a> of <a href="http://codecollege.ca" target="_blank">CodeCollege.ca</a></h5>
+<h5>Copyright &copy;<?php echo date('Y');?> <a href="https://gitlab.com/mahfuz380" target="_blank" style="text-decoration: none;">Al Mahfuz</a> </h5>
